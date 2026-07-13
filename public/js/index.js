68a87170a2ff9758caecc4c9c7ba20fc9cd5bc9d
K1dC0d3 <zion.samadebayo@gmail.com>
2026-07-13 19:10:01 -0400

    Added more assets and made the second section of the page

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+const navText = document.querySelectorAll("li>a");
+const introductionSection = document.getElementById("introduction-section");
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
@@ -7,4 +9,28 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenText = document.querySelectorAll('.hidden-text');
+
+hiddenText.forEach((p, index) => {
+  p.style.setProperty("--delay", `${0.7 + (index * 0.3)}s`);
+});
+
 hiddenElements.forEach((el) => observer.observe(el));
+hiddenText.forEach(el => observer.observe(el));
+
+window.addEventListener("scroll", (e) => {
+  const triggerRect = introductionSection.getBoundingClientRect();
+  const isInView = triggerRect.top < (window.innerHeight * 0.5) && triggerRect.bottom > 0;
+
+  if (isInView) {
+    navText.forEach((link) => {
+      link.style.color = "violet";
+      link.style.textShadow = "0px 0px 5px violet";
+    });
+  } else {
+    navText.forEach((link) => {
+      link.style.color = "skyblue";
+      link.style.textShadow = "0px 0px 5px skyblue";
+    });
+  }
+});
