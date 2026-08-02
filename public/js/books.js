def5e6281bfaa8c85fda11b038910d26a9b74502
K1dC0d3 <zion.samadebayo@gmail.com>
2026-08-02 19:49:34 -0400

    Optimization to book pages

--- public/js/books.js
+++ public/js/books.js
@@ -97,4 +97,14 @@ function animate(time) {
 
 }
 
+const hiddenElements = document.querySelectorAll(".hidden");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+hiddenElements.forEach((el) => observer.observe(el));
+
 animate();
